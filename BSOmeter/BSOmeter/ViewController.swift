c6fbbcc0e02517693545be735e5cc109f552e5fa
Shaheen Sharifian <shaheen.sharifian@gmail.com>
2015-10-17 22:53:50 -0700

    Created project and pod dependencies

---
+++ BSOmeter/BSOmeter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BSOmeter
+//
+//  Created by Shaheen Sharifian on 10/17/15.
+//  Copyright © 2015 Dubhacks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
