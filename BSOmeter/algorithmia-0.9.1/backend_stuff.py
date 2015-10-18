bffb1056ec00ce73f6def74be055c79e3d01012a
rosiezou <rosiezou@gmail.com>
2015-10-18 14:03:47 -0400

    text extraction & analysis - in progress

--- BSOmeter/algorithmia-0.9.1/backend_stuff.py
+++ BSOmeter/algorithmia-0.9.1/backend_stuff.py
@@ -1,7 +1,40 @@
 import Algorithmia
 
-apiKey = 'simfMcTKIsg3/P+fl/1ENYWrnQd1'
+# 1.1 URL to text
+# retrieves text file from URL
+# Str -> Str
+input = "your-URL-here"
+client = Algorithmia.client('simfMcTKIsg3/P+fl/1ENYWrnQd1')
+algo = client.algo('util/ExtractText/0.1.0')
+doc = algo.pipe(input)
 
-print Algorithmia.client(apiKey).algo('util/echo').pipe('echo this string')
 
-## Analysis Template
+# 1.2 Sentence detection
+# breaks down contents from 1.2
+# Str -> listof Str
+input = doc
+client = Algorithmia.client('simfMcTKIsg3/P+fl/1ENYWrnQd1')
+algo = client.algo('ApacheOpenNLP/SentenceDetection/0.1.0')
+sentences = algo.pipe(input)
+
+
+# 1.3 LDA - Tags and keywords
+# input: a list of string observations, and number of topics to generate
+# (listof Str) Num -> listof Str
+#input = [sentences,5]
+#client = Algorithmia.client('simfMcTKIsg3/P+fl/1ENYWrnQd1')
+#algo = client.algo('mallet/LDA/0.1.2')
+#keywords = algo.pipe(input)[0].keys()
+
+
+# 1.4 Sentiment Analysis
+# takes a string and gives a sentiment score
+# 0-4: very negative, negative, neutral, positive, very positive
+wholething = []
+for phrase in sentences:
+    input = phrase
+    client = Algorithmia.client('simfMcTKIsg3/P+fl/1ENYWrnQd1')
+    algo = client.algo('nlp/SentimentAnalysis/0.1.1')
+    sentiment = algo.pipe(input)
+    wholething = wholething + [sentiment]
+average = sum(wholething)/len(wholething)
