948d51d803ee7a85580b6a62d52e191c30676b97
rosiezou <rosiezou@gmail.com>
2015-10-18 16:21:46 -0400

    sentiment analysis - final

--- BSOmeter/algorithmia-0.9.1/backend_stuff.py
+++ BSOmeter/algorithmia-0.9.1/backend_stuff.py
@@ -3,28 +3,28 @@ import Algorithmia
 # 1.1 URL to text
 # retrieves text file from URL
 # Str -> Str
-input = "your-URL-here"
+input = "http://www.cnn.com/2015/10/18/politics/donald-trump-jeb-bush-foreign-policy/index.html"
 client = Algorithmia.client('***REMOVED***')
 algo = client.algo('util/ExtractText/0.1.0')
 doc = algo.pipe(input)
 
 
-# 1.2 Sentence detection
+# 1.2 Summarizer
+# summarizes info from 1.1
+# Str -> Str
+input = doc
+client = Algorithmia.client('***REMOVED***')
+algo = client.algo('nlp/Summarizer/0.1.2')
+summarized = algo.pipe(input)
+
+
+# 1.3 Sentence detection
 # breaks down contents from 1.2
 # Str -> listof Str
-input = doc
+input = summarized
 client = Algorithmia.client('***REMOVED***')
 algo = client.algo('ApacheOpenNLP/SentenceDetection/0.1.0')
 sentences = algo.pipe(input)
-
-
-# 1.3 LDA - Tags and keywords
-# input: a list of string observations, and number of topics to generate
-# (listof Str) Num -> listof Str
-#input = [sentences,5]
-#client = Algorithmia.client('***REMOVED***')
-#algo = client.algo('mallet/LDA/0.1.2')
-#keywords = algo.pipe(input)[0].keys()
 
 
 # 1.4 Sentiment Analysis
@@ -37,4 +37,4 @@ for phrase in sentences:
     algo = client.algo('nlp/SentimentAnalysis/0.1.1')
     sentiment = algo.pipe(input)
     wholething = wholething + [sentiment]
-average = sum(wholething)/len(wholething)
+average = float(sum(wholething))/len(wholething)
