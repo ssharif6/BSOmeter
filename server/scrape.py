150b2bb57b0418cfda346cbcd16eb591bc9596c2
Jules Mazur <julesmazur@gmail.com>
2015-10-18 16:19:45 -0700

    Added scraper that pulls stuff from politifact and dumps it into a dummy training database

---
+++ server/scrape.py
@@ -0,0 +1,32 @@
+import re
+import json
+import requests
+
+import pg8000 as pg8k
+
+from bs4 import BeautifulSoup
+
+conn = pg8k.connect(
+            host="politidb.cm8mhypepst9.us-west-2.rds.amazonaws.com",
+            port=5432,
+            user="***REMOVED***",
+            password="***REMOVED***",
+            database="postgres"
+        )
+
+c = conn.cursor()
+c.execute('CREATE TABLE IF NOT EXISTS Lies (statement VARCHAR(2048));')
+conn.commit()
+c.execute('CREATE TABLE IF NOT EXISTS Truths (statement VARCHAR(2048));')
+conn.commit()
+
+docs = json.loads(requests.get('http://www.politifact.com/api/statements/truth-o-meter/json/?n=250').text)
+
+for doc in docs:
+    if doc['ruling']['ruling'].lower() in ['half-true', 'mostly true', 'true']:
+        c.execute("INSERT INTO Truths (statement) VALUES (%s)", (re.sub(r'&nbsp;', '\\n', re.sub(r'(\<p\>)?&quot;(\</p\>)?', '', doc['statement'])),))
+        conn.commit()
+    else:
+        c.execute("INSERT INTO Lies (statement) VALUES (%s)", (re.sub(r'&nbsp;', '\\n', re.sub(r'(\<p\>)?&quot;(\</p\>)?', '', doc['statement'])),))
+        conn.commit()
+
