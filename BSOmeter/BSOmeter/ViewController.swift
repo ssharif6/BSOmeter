d8435fe8bc6ac208df235a0f5a083e5fb7565383
Adam Sloma <adamsloma4321@gmail.com>
2015-10-25 11:17:30 -0700

    Beginning to improve UI

--- BSOmeter/BSOmeter/ViewController.swift
+++ BSOmeter/BSOmeter/ViewController.swift
@@ -21,5 +21,25 @@ class ViewController: UIViewController {
     }
 
 
+    @IBOutlet var checkBSButton: UIButton!
+    @IBOutlet var BSOMeterLabel: UILabel!
+    
+    @IBAction func switchToInputSelect(sender: AnyObject) {
+//        //fade out buttons and set them to nil
+//        UIView.animateWithDuration(0.6, animations: {
+//            self.checkBSButton.frame.origin.y+=200
+//            self.checkBSButton.alpha = 0.0
+//            
+//            self.BSOMeterLabel.frame.origin.y-=200
+//            self.BSOMeterLabel.alpha = 0.0
+//        })
+//        
+//        self.checkBSButton = nil;
+//        self.BSOMeterLabel = nil;
+        
+        let format = FormatSelectViewController()
+            }
+    
+    
 }
 
