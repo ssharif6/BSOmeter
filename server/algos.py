c72f221e3c32f6e7047c9502f6c59bf85fd37ac0
Adam Sloma <adamsloma4321@gmail.com>
2015-10-27 17:39:45 -0700

    This commit is a commit

--- server/algos.py
+++ server/algos.py
@@ -7,11 +7,11 @@ import pg8000 as pg8k
 from db_scripts import Db
 
 conn = pg8k.connect(
-            host="politidb.cm8mhypepst9.us-west-2.rds.amazonaws.com",
+            host="",
             port=5432,
-            user="trumpster",
-            password="bushdidnothingwrong",
-            database="postgres"
+            user="",
+            password="",
+            database=""
         )
 
 d = Db(conn)
@@ -21,12 +21,10 @@ def calculateP_NF_T(name, topic):
     t = d.getAverageNewsSourceRepByTopic(name, topic)
 
 def calculateP_BS_W(pname, topic, keyword):
-    r = getBayesianData(pname, topic, keyword)
+    r = d.getBayesianData(pname, topic, keyword)
     p_w_bs = r['kw']['bs'] / r['kw']['total']
     p_bs = r['no-kw']['bs'] / r['no-kw']['total']
     p_w_t = (r['kw']['total'] - r['kw']['bs']) / r['kw']['total']
     p_t = (r['no-kw']['total'] - r['no-kw']['bs']) / r['no-kw']['total']
     return (p_w_bs * p_bs) / (p_w_bs * p_bs + p_w_t * p_t)
-
-
 
