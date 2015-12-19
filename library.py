e9827c3eb53396a00e741cb33a922347b748ec82
rosiezou <rosiezou@gmail.com>
2015-12-18 23:42:31 -0500

    started a library of buzzwords

---
+++ library.py
@@ -0,0 +1,39 @@
+# list of buzzwords/topics of discussion during presidential debates:
+buzzwords = [
+"planned parenthood",
+"immigration",
+"mexico",
+"terrorism",
+"isis",
+"foreign policy",
+"economy",
+"military",
+"gun control",
+"gun violence",
+"education",
+"climate change",
+"national security",
+"cyber security",
+"undocumented immigrants",
+"air forces",
+"ground forces"]
+
+# starting words for opinions
+opinions = [
+"i think",
+"i believe",
+"i'm afraid",
+]
+
+# starting words for facts
+facts = [
+"it is my undertanding that",
+"i am aware that",
+"as far as i'm concerned",
+"as far as i'm aware",
+"like obama never did",
+"obama's government",
+"under obama",
+"for the past seven years",
+"for the past eight years",
+]
